0b14229e55d85b474508267309283d3a45775c6f
Or Kravitz <imorkravitz@gmail.com>
2022-05-02 12:22:06 +0300

    fix bug

--- ticTacToeAi.py
+++ ticTacToeAi.py
@@ -23,8 +23,6 @@ def init_board():
     board[8] = ' '
     board[9] = ' '
 
-# print_b(board)
-
 
 def space_is_free(position):
     if board[position] == ' ':
@@ -37,6 +35,8 @@ def check_for_tie():
     for i in board.keys():
         # still available space
         if board[i] == ' ':
+            return False
+        elif check_is_win():
             return False
     return True
 
@@ -84,55 +84,65 @@ def check_which_pos_won(pos):
 
 
 def insert_letter(letter, position):
-    global player1_score, player2_score, computer_score, flag
+    global player1_score, player2_score, computer_score, flag, human_score
     if space_is_free(position):
         board[position] = letter
         print_b(board)
         if check_for_tie():
             print("Tie!")
-            player1_score += 1
-            player2_score += 1
-            computer_score += 1
+            if int(game_option) == 1:
+                player1_score += 1
+                player2_score += 1
+            elif int(game_option) == 2:
+                computer_score += 1
+                human_score += 1
             flag = False
-            return
+            init_board()
+
         elif check_is_win():
             if letter == 'X':
                 if int(game_option) == 1:
                     print(player1_name + " Win!")
                     player1_score += 2
-                    flag = False
                 elif int(game_option) == 2:
                     print("Player Win!")
-                    player1_score += 2
-                    flag = False
-
-                return
+                    human_score += 2
+                flag = False
             else:
                 if int(game_option) == 1:
                     print(player2_name + " Win!")
                     player2_score += 2
-                    flag = False
                 elif int(game_option) == 2:
                     print("Computer Win!")
                     computer_score += 2
-                    flag = False
-
-                return
+                flag = False
     else:
         print("this position is taken, try again!")
         position = int(input("Enter a new position (1-9): "))
         insert_letter(letter, position)
-        return
+    return
 
 
 player1 = 'X'
 player2 = 'O'
 computer = 'O'
+human = 'X'
+
+human_score = 0
+player1_score = 0
+player2_score = 0
+computer_score = 0
 
 
 def player_move():
     position = int(input(player1_name + "'s turn. Enter the position for 'X' :"))
     insert_letter(player1, position)
+    return
+
+
+def human_move():
+    position = int(input("Human. Enter the position for 'X' :"))
+    insert_letter(human, position)
     return
 
 
@@ -201,16 +211,16 @@ def show_score():
 
 
 # option to check table score status by typing ShowScore of player vs computer
-# def show_score2():
-#     global HUMAN, COMPUTER
-#     score = input("Type 'ShowScore' in order to see score status\nor 'any key' to continue\n")
-#     if score == "ShowScore":
-#         print("Table Score:")
-#         print("    " + str(human_score) + ":    " + str(computer_score))
-#         print(HUMAN + " : " + COMPUTER + "\n")
-#     else:
-#         return
-#     return
+def show_score2():
+    score = input("Type 'ShowScore' in order to see score status\nor 'any key' to continue\n")
+    if score == "ShowScore":
+        print("\nTable Score:")
+        print("Human : Computer")
+        print(str(human_score) + " : " + str(computer_score) + "\n")
+    else:
+        return
+    return
+
 
 game_option = input("Choose (1) to play  >>> player Vs player\n"
                     "Choose (2) to play >>> player Vs computer\n:")
@@ -223,6 +233,15 @@ def play_game1():
         player_move()
         if flag is not False:
             player2_move()
+    return
+
+
+def play_game2():
+    while not check_is_win():
+        computer_move()
+        if flag is not False:
+            human_move()
+    return
 
 
 if int(game_option) == 1:
@@ -230,9 +249,6 @@ if int(game_option) == 1:
     name2 = ""
     player1_name = str(input("please enter player 1 name: " + name1))
     player2_name = str(input("please enter player 2 name: " + name2))
-    player1_score = 0
-    player2_score = 0
-    computer_score = 0
 
     choice = "y"
     while choice == "y" or choice == "Y":
@@ -252,7 +268,24 @@ if int(game_option) == 1:
             break
     print("Bye Bye")
 
-# while not check_is_win():
-#     player_move()
-#     # player2_move()
-#     computer_move()
+if int(game_option) == 2:
+    choice = "y"
+    while choice == "y" or choice == "Y":
+        play_game2()
+        print("********************")
+        print("Starting a new round...\n")
+        show_score2()
+        choice = input("if want to  play again press (y) or (n) to quit!")
+        if choice == 'y' or choice == 'Y':
+            flag = True
+            init_board()
+            check_is_win() == False
+
+        elif choice == "n" or choice == "N":
+            game_time = False
+            print("Game Over!")
+            break
+    print("Bye Bye")
+
+
+
